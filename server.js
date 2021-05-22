56721535d3a16b73a9da88002c36da1b861235be
LyubooShv <liubomirshivachev@gmail.com>
2021-05-22 19:16:33 +0300

    server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const shortid = require("shortid");
 const app = express();
 app.use(express.json());
 
-// const CONNECTION_URL = 'mongodb+srv://LyuboShv:Poortsmouth1@cluster0.jg4jz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// const MONGODB_URL = 'mongodb+srv://LyuboShv:Poortsmouth1@cluster0.jg4jz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 app.use("/", express.static(__dirname + "/build"));
 app.get("/", (req, res) => res.sendFile(__dirname + "/build/index.html"));
-mongoose.connect( process.env.MONGODB_URL || "mongodb://localhost/brewed-drinks-info-db", {
+mongoose.connect( 'mongodb+srv://LyuboShv:Poortsmouth1@cluster0.jg4jz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' || "mongodb://localhost/brewed-drinks-info-db", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
